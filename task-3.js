c78ae94feeeba23b3a26799c133b0ae25e9e4bfd
MariiaMelychyn <lv080191mmi75@gmail.com>
2021-08-05 20:08:57 +0300

    Update task-3.js

--- task-3.js
+++ task-3.js
@@ -3,19 +3,20 @@ const randomIntegerFromInterval = (min, max) => {
   };
   
   const makeTransaction = (transaction, onSuccess, onError) => {
+    return new Promise((resolve, reject) => {
     const delay = randomIntegerFromInterval(200, 500);
   
     setTimeout(() => {
       const canProcess = Math.random() > 0.3;
   
       if (canProcess) {
-        onSuccess(transaction.id, delay);
+        resolve({id: transaction.id, time:delay});
       } else {
-        onError(transaction.id);
+        reject(transaction.id);
       }
     }, delay);
-  };
-  
+  });
+};
   const logSuccess = (id, time) => {
     console.log(`Transaction ${id} processed in ${time}ms`);
   };
